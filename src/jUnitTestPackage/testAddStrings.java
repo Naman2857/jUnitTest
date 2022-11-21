479c28276662f62355d7e98bddcda27fbd8ebc46
HP <HP@10.12.21.134>
2022-11-21 15:51:36 +0530

    hUnitTest

---
+++ src/jUnitTestPackage/testAddStrings.java
@@ -0,0 +1,15 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddStrings {
+
+	@Test
+	public void test() {
+		jUnitFunctions junit = new jUnitFunctions();
+		String result=junit.addStrings("Hello","World");
+		assertEquals("HelloWorld",result);
+	}
+}
