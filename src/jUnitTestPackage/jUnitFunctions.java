15498d3e186c216d09e84b921bf4a76472525d93
HP <HP@LAPTOP-UERH0IIN.mshome.net>
2022-11-21 14:54:17 +0530

    jUnittest

--- src/jUnitTestPackage/jUnitFunctions.java
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -1,23 +1,10 @@
 package jUnitTestPackage;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class jUnitFunctions {
-	static void addinteger() {
-		Scanner s = new Scanner(System.in);
-		int a = s.nextInt();
-		int b = s.nextInt();
-		System.out.println(a+b);
+	public int addNumbers(int num1, int num2) {
+		return num1 + num2;
 	}
-	static void addstring(){
-		Scanner t = new Scanner(System.in);
-		String c = t.next();
-		String d = t.next();
-		System.out.println(c+d);
-	}
-	public static void main(String[] args) {
-//		jUnitFunctions obj = new jUnitFunctions();
-//		obj.addinteger();
-//		obj.addstring();
-		addinteger();
-		addstring();
+	public String addStrings(String s1, String s2) {
+		return s1+s2;
 	}
 }
